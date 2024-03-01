28330c2eaf4b0a9c2e4b3700c3f298974293e8e6
OmarMWarraich <earthling986@gmail.com>
2024-03-02 02:15:46 +0500

    feat: Resources, EC2 and Routable

---
+++ ec2_resource.tf
@@ -0,0 +1,27 @@
+# Create EC2 Instances
+
+resource "aws_instance" "web1" {
+  ami = "ami-0c7217cdde317cfec"
+  instance_type = "t2.micro"
+  key_name = "twotier-key-pair"
+  availability_zone = "us-east-1a"
+  vpc_security_group_ids = [aws_security_group.alb_sg.id]
+  subnet_id = aws_subnet.public_subnet_1.id
+  associate_public_ip_address = true
+  tags = {
+    Name = "web1_instance"
+  }
+}
+
+resource "aws_instance" "web2" {
+  ami = "ami-0c7217cdde317cfec"
+  instance_type = "t2.micro"
+  key_name = "twotier-key-pair"
+  availability_zone = "us-east-1b"
+  vpc_security_group_ids = [aws_security_group.alb_sg.id]
+  subnet_id = aws_subnet.public_subnet_2.id
+  associate_public_ip_address = true
+  tags = {
+    Name = "web2_instance"
+  }
+}
