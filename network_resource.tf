584595de3841aa5e893c9010e1587ef43599dbd1
OmarMWarraich <earthling986@gmail.com>
2024-03-02 01:03:49 +0500

    Init Com w/ Provider, Security Resource and Network Resource

---
+++ network_resource.tf
@@ -0,0 +1,70 @@
+# Create VPC
+resource "aws_vpc" "vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "vpc-project"
+  }
+}
+
+# Create Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "igw-project"
+  }
+}
+
+# Create Two Public Subnets
+
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-1"
+  }
+
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-2"
+  }
+
+}
+
+# Create 2 Private Subnets
+
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "private-subnet-1"
+  }
+
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.4.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "private-subnet-2"
+  }
+
+}
---
+++ security_resource.tf
@@ -0,0 +1,82 @@
+# Security Group for ALB
+resource "aws_security_group" "alb_sg" {
+  name        = "alb-sg"
+  description = "Security group for ALB"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = "0"
+    to_port     = "0"
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = "0"
+    to_port     = "0"
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb_sg"
+  }
+}
+
+# Create ALB
+
+resource "aws_lb" "project_alb" {
+  name               = "alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
+}
+
+# Create ALB Target Group
+
+resource "aws_lb_target_group" "project_tg" {
+  name     = "project_tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.vpc.id
+  depends_on = [
+    aws_vpc.vpc
+  ]
+}
+
+# Create Target Attachments
+
+resource "aws_lb_target_group_attachment" "project_tg_attach" {
+  target_group_arn = aws_lb_target_group.project_tg.arn
+  target_id        = aws_instance.web1.id
+  port             = 80
+
+  depends_on = [
+    aws_instance.web1
+  ]
+}
+
+resource "aws_lb_target_group_attachment" "project_tg_attach2" {
+  target_group_arn = aws_lb_target_group.project_tg.arn
+  target_id        = aws_instance.web2.id
+  port             = 80
+
+  depends_on = [
+    aws_instance.web2
+  ]
+}
+
+# Create Listener
+
+resource "aws_lb_listener" "project_listener" {
+  load_balancer_arn = aws_lb.project_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.project_tg.arn
+  }
+}
